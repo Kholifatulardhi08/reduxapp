23f25ededf0bcd0b09b07a76210fb73c91d04365
Kholifatulardhi08 <ardhi.kholifatul8@gmail.com>
2022-04-21 02:22:41 -0700

    Lanjutanya
    Signed-off-by: Kholifatulardhi08 <ardhi.kholifatul8@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import MainReducer from './reducers/MainReducer'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import registerServiceWorker from './registerServiceWorker';
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => 
+f)(createStore)(MainReducer)
+ReactDOM.render(<Provider store={store}>
+<App />
+</Provider>
+, document.getElementById('root'));
+registerServiceWorker();
